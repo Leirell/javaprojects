167b205c6cf857b404d44f80f607f6f1d366250d
Leirell <leire_gallego_jimenez@outlook.com>
2022-11-15 17:18:36 +0100

    initial version

--- src/entrega/repaso1.java
+++ src/entrega/repaso1.java
@@ -1,4 +1,31 @@
 package entrega;
 
+import java.util.Scanner;
 public class repaso1 {
+    private static final int LIMITE_INFERIOR = 5;
+    private static final int LIMITE_SUPERIOR = 17;
+    public static void main(String[] args) {
+        Scanner readInput = new Scanner(System.in);
+
+        int numero = 0;
+        boolean continuar = true;
+
+        do {
+            System.out.println("Introduzca un número dentro del rango");
+            numero = readInput.nextInt();
+
+            if ((numero >= LIMITE_INFERIOR) && numero <= LIMITE_SUPERIOR){
+                System.out.println("Enhorabuena has acertado !!");
+            }
+            else{
+                System.out.println("Has fallado");
+            }
+
+            System.out.println("¿Desea continuar?");
+            System.out.println("True = Sí  False = No");
+            continuar = readInput.nextBoolean();
+        }while (continuar);
+        readInput.close();
+    }
 }
+
--- src/entrega/repaso2.java
+++ src/entrega/repaso2.java
@@ -1,4 +1,25 @@
 package entrega;
 
+import java.util.Scanner;
 public class repaso2 {
+    public static void main(String[] args) {
+        Scanner readInput = new Scanner(System.in);
+
+        int numeroCaracteres;
+        String caracter;
+
+        System.out.println("¿Cuántos caracteres quiere escribir?");
+        numeroCaracteres = readInput.nextInt();
+
+        for (int i = 1; i <= numeroCaracteres; i++){
+            if ((i % 2 == 0) || (i % 3 == 0)){
+                caracter = "€";
+            }
+            else{
+                caracter = "$";
+            }
+            System.out.println(i + " - " + caracter);
+        }
+        readInput.close();
+    }
 }
